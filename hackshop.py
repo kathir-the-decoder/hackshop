dc4b507e19e751a9a6ed0d722bf2f31c3504862d
kathir-the-decoder <kathirvelsathy07@gmail.com>
2024-11-23 16:01:11 +0530

    Create hackshop.py

---
+++ hackshop.py
@@ -0,0 +1,30 @@
+def greatest(num1,num2):
+ if num1>num2:
+  return num1
+ else: 
+  return num2
+num1=int(input("Enter the num1: "))
+num2=int(input("Enter the num2: "))
+print(greatest(num1,num2),"is gratest")
+
+def sub (a,b):
+    return a-b
+s=sub(5,3)
+print(s)
+  
+def s():
+    return 5
+    print(s())
+ 
+def sub():
+     print("hello")
+
+def vote(age):
+    if age>18:
+        print("Eligible for vote")
+    else:
+        print("Not eligible for vote")
+        
+age=int(input("Enter your age: "))
+vote(age)
+        
